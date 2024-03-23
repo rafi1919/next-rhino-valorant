e2831549740180e772fe64ee199fa380f56869d1
rafi1919 <rafifourze1904@gmail.com>
2024-03-23 14:27:41 +0700

    bug fix

--- pages/News/[newsId]/index.tsx
+++ pages/News/[newsId]/index.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import LayoutPage from '@/templates/LandingPage/components/Layout';
-import { useGetNewsDetail } from '../hooks';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import LayoutPage from "@/templates/LandingPage/components/Layout";
+import { useGetNewsDetail } from "../hooks";
 
-const Index = () => {
+export default function Index() {
   const router = useRouter();
   const { newsId } = router.query;
   const [stateData, setStateData] = useState<any[]>([]);
-  const { data, loading } = useGetNewsDetail({id: newsId as string});
+  const { data, loading } = useGetNewsDetail({ id: newsId as string });
 
   useEffect(() => {
     if (data) {
@@ -18,28 +18,40 @@ const Index = () => {
 
   return (
     <>
-      <LayoutPage  section={stateData.length > 0 ? stateData[0].title : ''} arrow>
-        <div className='my-5'>
-
+      <LayoutPage
+        section={stateData.length > 0 ? stateData[0].title : ""}
+        arrow
+      >
+        <div className="my-5">
           {loading ? (
             <p>Loading...</p>
           ) : stateData.length > 0 ? (
             <>
-              <img src={stateData[0].banner.url} className='w-full' />
-              <p className='text-[50px] text-blackGrey font-bold'>{stateData[0].description}</p>
-              <p className='text-[50px] text-blackGrey font-bold'>{stateData[0].pathSlug}</p>
-              {stateData[0]?.article_body?.map((item:any, index:any) => (
-                <div key={index} dangerouslySetInnerHTML={{ __html: item.rich_text_editor.rich_text_editor }} className='my-10' />
+              <img src={stateData[0].banner.url} className="w-full" />
+              <p className="text-[50px] text-blackGrey font-bold">
+                {stateData[0].description}
+              </p>
+              <p className="text-[50px] text-blackGrey font-bold">
+                {stateData[0].pathSlug}
+              </p>
+              {stateData[0]?.article_body?.map((item: any, index: any) => (
+                <div
+                  key={index}
+                  dangerouslySetInnerHTML={{
+                    __html: item.rich_text_editor.rich_text_editor,
+                  }}
+                  className="my-10"
+                />
               ))}
               {/* Render other details of the article */}
             </>
           ) : (
-            <p>{!loading && !data && newsId ? "Article not found" : "Loading..."}</p>
+            <p>
+              {!loading && !data && newsId ? "Article not found" : "Loading..."}
+            </p>
           )}
         </div>
       </LayoutPage>
     </>
   );
 }
-
-export default Index;
